826f27ad26fba44a138d33f7594294aa5e7d1b98
Kasi Reddy Golamari <kasireddyg128@gmail.com>
2024-07-11 16:27:47 +0530

    yes

---
+++ Vote.java
@@ -0,0 +1,10 @@
+class Vote{
+public static void main(String[] args){
+    int age=29;
+   if(age>18){
+     System.out.println("The person is eligible for vote ");
+  }
+   else{
+      System.out.println("not eligible"); 
+}
+}
